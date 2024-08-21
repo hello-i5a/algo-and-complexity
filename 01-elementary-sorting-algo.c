531106337b0c62c08dd614e8d5a73f64d772dc4f
hello-isa <isabeljoyadriatico@gmail.com>
2024-08-22 03:47:16 +0800

    Create 01-elementary-sorting-algo.c

---
+++ 01-elementary-sorting-algo.c
@@ -0,0 +1,16 @@
+/*
+Elementary Sorting Algorithms
+1. Bubble sort
+2. Selection sort
+3. Insertion sort
+
+A collection of elements -> Array implementation
+Before you sort, you swap -> temporary variable
+
+Bubble sort:
+The largest value bubble up (or goes) to the top (or tail).
+* First loop: i -> from N to 0
+* Inner loop: j -> from 0 to i-1
+* arr[j] > arr[j + 1] -> swap the two values
+
+*/
