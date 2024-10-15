59ac569f9d8943a7ddfeaa404fb71d1936fb087f
hello-isa <isabeljoyadriatico@gmail.com>
2024-10-15 21:12:39 +0800

    Create 07-avl-tree-bst-tree.c

---
+++ 07-avl-tree-bst-tree.c
@@ -0,0 +1,21 @@
+/*
+Refresher Concepts
+Lists == Linear
+Tree == Non-linear
+
+NOTE:
+* Review tree terminologies from DSA Summer Term
+
+
+Binary Search Tree Concepts
+Binary Search Tree (BST): A tree that has at most 2 children nodes for each parent node
+                            LC < P < RC
+Operation (i.e. insertion, deletion, search) time complexity: O(log n) average-case while O(n) for the worst-case
+    Tree becomes unbalanced == Worst-case time complexity
+
+
+AVL Tree Concepts
+AVL Tree: A binary search tree (BST) that is self-balancing.
+Balance factor: -1, 0 or 1 for every node
+                To maintain the O(log n) time complexity for insertion, deletion and search operations
+*/
